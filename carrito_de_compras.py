a0dea885bd1ecb8238462b97175acb9b7b024079
Luis Marin <luis2525063@hybridge.education>
2026-01-20 08:16:41 -0500

    se creó una prueba para el carrito de compras

---
+++ carrito_de_compras.py
@@ -0,0 +1,8 @@
+#creo un documento donde voy a probar los carritos de compras
+
+class Carrito:
+    def __init__(self, producto):
+        self.producto = producto
+
+    def visualizar_carrito(self):
+        print(self.producto)
